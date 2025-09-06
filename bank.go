4c68c0953b644ba3a3ce30301aa399ccf3f2b400
Aarish Jafri <aarishhassan98@gmail.com>
2025-09-07 03:05:49 +0500

    feat: Implemented new switch cases as well with for loop and also added multiple functions as well and how to write and read a file in the function.

--- bank.go
+++ bank.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64 {
+	data, _ := os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance, _ := strconv.ParseFloat(balanceText, 64)
+	return balance
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0664)
+}
 
 func main() {
-	var accountBalance = 1000.00
+	var accountBalance = getBalanceFromFile()
 
 	for {
 		fmt.Println("Welcome to go bank !")
@@ -17,9 +35,11 @@ func main() {
 		fmt.Printf("Your choice: ")
 		fmt.Scan(&choice)
 
-		if choice == 1 {
+		switch choice {
+		case 1:
 			fmt.Println("Your balance is:", accountBalance)
-		} else if choice == 2 {
+
+		case 2:
 			fmt.Print("How much would you like to deposit ? ")
 			var depositAmount float64
 
@@ -32,7 +52,9 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("The account balance have been updated ! New Amount is:", accountBalance)
-		} else if choice == 3 {
+			writeBalanceToFile(accountBalance)
+
+		case 3:
 			fmt.Print("How much would you like to withdraw ? ")
 			var withdrawAmount float64
 
@@ -48,15 +70,58 @@ func main() {
 			}
 			accountBalance -= withdrawAmount
 			fmt.Println("The account balance have been updated ! New Amount is:", accountBalance)
-		} else if choice == 4 {
+			writeBalanceToFile(accountBalance)
+
+		case 4:
 			fmt.Println("Exited your account successfully !")
-			break
-		} else {
-			fmt.Println("Wrong choice selected !")
-			break
+			return
+
+		default:
+			fmt.Println("Wrong choice selected. Exitinng ...")
+			return
+			// break
 		}
+
+		// if choice == 1 {
+		// 	fmt.Println("Your balance is:", accountBalance)
+		// } else if choice == 2 {
+		// 	fmt.Print("How much would you like to deposit ? ")
+		// 	var depositAmount float64
+
+		// 	fmt.Scan(&depositAmount)
+
+		// 	if depositAmount <= 0 {
+		// 		fmt.Println("Invalid amount entered, must be greater then 0:", depositAmount)
+		// 		continue
+		// 	}
+
+		// 	accountBalance += depositAmount
+		// 	fmt.Println("The account balance have been updated ! New Amount is:", accountBalance)
+		// } else if choice == 3 {
+		// 	fmt.Print("How much would you like to withdraw ? ")
+		// 	var withdrawAmount float64
+
+		// 	fmt.Scan(&withdrawAmount)
+
+		// 	if withdrawAmount <= 0 {
+		// 		fmt.Println("Invalid amount entered, must be greater then 0:", accountBalance)
+		// 		continue
+		// 	}
+		// 	if withdrawAmount > accountBalance {
+		// 		fmt.Println("You can not withdraw more then you have:", accountBalance)
+		// 		continue
+		// 	}
+		// 	accountBalance -= withdrawAmount
+		// 	fmt.Println("The account balance have been updated ! New Amount is:", accountBalance)
+		// } else if choice == 4 {
+		// 	fmt.Println("Exited your account successfully !")
+		// 	break
+		// } else {
+		// 	fmt.Println("Wrong choice selected !")
+		// 	break
+		// }
 	}
 
-	fmt.Println("Thank for choosing our bank !")
+	// fmt.Println("Thank for choosing our bank !")
 
 }
